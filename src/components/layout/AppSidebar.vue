09abc7ea4b4b76978c081c15e28956c83d1392e6
Your Name <your.email@example.com>
2025-10-20 15:06:58 -0700

    Added a categories bar

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -55,6 +55,18 @@
         >
       </router-link>
       <router-link
+        to="/categories"
+        class="flex items-center py-3 px-3 text-slate-300 no-underline transition-all duration-200 ease-in-out relative cursor-pointer hover:bg-slate-600 hover:text-white"
+        :class="{ 'bg-brand-blue text-white': $route.path === '/categories' }"
+      >
+        <div class="min-w-[30px] h-[30px] mr-3 flex items-center justify-center">🗃️</div>
+        <span
+          class="whitespace-nowrap transition-opacity duration-300 ease-in-out"
+          :class="{ 'opacity-100': isHovered, 'opacity-0': !isHovered }"
+          >Categories</span
+        >
+      </router-link>
+      <router-link
         to="/documents"
         class="flex items-center py-3 px-3 text-slate-300 no-underline transition-all duration-200 ease-in-out relative cursor-pointer hover:bg-slate-600 hover:text-white"
         :class="{ 'bg-brand-blue text-white': $route.path === '/documents' }"
